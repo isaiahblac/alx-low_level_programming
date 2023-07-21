eda05f73de6b3f94c8afb713cf65d88dfb335d51
isaiahblac <isaiahkayode9@gmail.com>
2023-07-20 19:43:58 -0700

    square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - printing a square
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	j = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (j < size)
+	{
+		i = 0;
+		while (i < size)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
