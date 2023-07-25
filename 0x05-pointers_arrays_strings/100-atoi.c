d7bd4f5b1c2b2c34fab44cddf9c50ba12037d48b
isaiahblac <isaiahkayode9@gmail.com>
2023-07-25 16:01:09 -0700

    sacrifice num

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * _atoi - converts string to integer
+ * @s: An input string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			result = result * 10 + *s - '0';
+		}
+
+		else if (null_flag)
+			break;
+		s++;
+	}
+
+	if (sign < 0)
+		result = (-result);
+
+	return (result);
+}
