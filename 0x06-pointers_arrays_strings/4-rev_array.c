6d7f41d75983ca5d39840dc160a8c0a6fb7b25c5
isaiahblac <isaiahkayode9@gmail.com>
2023-07-28 09:44:08 -0700

    paranoid in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the variable of an array of integers
+ * @a: array pointer
+ * @n: number of elements in array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n / 2 ; i++)
+	{
+		j = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
+	}
+}
