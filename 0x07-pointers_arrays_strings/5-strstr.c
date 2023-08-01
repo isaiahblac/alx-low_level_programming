6ad00501bfc449982ebd7f78ba7e395f3a29dec2
isaiahblac <isaiahkayode9@gmail.com>
2023-08-01 10:06:02 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates needle in haystack
+ * @haystack: pointer to the substring to search in
+ * @needle: pointer to the substing to locate
+ * Return: char found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; haystack[a] != '\0' ; a++)
+	{
+		for (b = 0 ; needle[b] != '\0' && haystack[a + b] == needle[b] ; b++)
+		{
+			if (needle[b] != '\0')
+				return (&haystack[a]);
+		}
+	}
+	return (NULL);
+}
+
