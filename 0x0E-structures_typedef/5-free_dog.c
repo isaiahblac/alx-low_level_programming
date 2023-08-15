0731e31ba0b63f7f9e9a65e8784260dd23388589
isaiahblac <isaiahkayode9@gmail.com>
2023-08-15 15:47:28 -0700

    legs of dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog
+ * @d: pointer to d
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
