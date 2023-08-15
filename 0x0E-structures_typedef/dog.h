3518825084f2fbf062f64452ae712b09ee317ab8
isaiahblac <isaiahkayode9@gmail.com>
2023-08-15 13:05:25 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - a structure for types of variable
+ * @name: pointer to name
+ * @age: age of dog
+ * @owner: pointer to owner
+ */
+
 struct dog
 {
 	char *name;
