f1152bb443478ae13d7d3f2ae1db6c88e3638632
isaiahblac <isaiahkayode9@gmail.com>
2023-08-17 17:06:57 -0700

    value of variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,22 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_char(va_list arg);
+void print_str(va_list arg);
+
+/**
+ * struct printTypeStruct - structure define printTypeStruct
+ * @type: type of arg
+ * @printer: pointer to function
+ */
+typedef struct printTypeStruct
+{
+char *type;
+void (*printer)(va_list);
+} printTypeStruct;
+
 
 
 #endif
